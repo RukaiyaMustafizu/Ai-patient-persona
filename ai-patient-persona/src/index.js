4560255beec420ab4af47d4ce102c1bdf059a120
RukaiyaMustafizu <rukaiyamustafizu@icloud.com>
2025-03-23 18:39:49 +0100

    did some corrections in the index.js

--- ai-patient-persona/src/index.js
+++ ai-patient-persona/src/index.js
@@ -1,18 +1,18 @@
+// This is the main entry point of the application.
+// This file is responsible for rendering the App component and wrapping it with the Router component.
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route path="/" component={App} />
+      {/* Add other routes if needed */}
+    </Switch>
+  </Router>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
